ff572d2bc47554e8df37144139d050c259f9c739
MXTIGV187 <shirko.mycik@ya.ru>
2023-04-27 22:12:31 +0300

    Добавьте файлы проекта.

---
+++ Game.cpp
@@ -0,0 +1,11 @@
+#include <SDL.h>
+
+int lasttime = SDL_GetTicks();
+int newtime;
+int dt = 0;
+
+
+// eto v igrovoi chikl nado.
+//newtime = SDL_GetTicks();
+//delta_time = nemtime - lasttime;
+//lastlime = newtime;
